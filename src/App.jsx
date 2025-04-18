4059c545466054d12ff4d368d3c36821fa110bcb
rbalendra <rj.balendra@gmail.com>
2025-04-18 23:51:14 +1000

    randomise colors - event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
 import EsignatureApp from './Projects/01-e-signature-app/EsignatureApp';
+import RandomiseColors from './Projects/02-randomisecolors/RandomiseColors';
 
 function App() {
-	return <EsignatureApp />;
+	return (
+		<div className='App'>
+			{/* <EsignatureApp />; */}
+			<RandomiseColors />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Projects/02-randomisecolors/RandomiseColors.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Title from '../01-e-signature-app/components/Title';
+
+const RandomiseColors = () => {
+	const handleClick = (e) => {
+		e.target.style.background = getRandomColor();
+	};
+
+	const handleSecClick = (e) => {
+		// console.log(e.target);
+		// console.log(getRandomColor());
+		let body = document.querySelector('body');
+		body.style.background = getRandomColor();
+		// e.target.style.backgroundColor = getRandomColor();
+	};
+
+	const getRandomColor = () => {
+		//here we are creating random color values
+		const letters = '0123456789ABCDEF';
+		let colors = '#';
+		for (let i = 0; i < 6; i++) {
+			colors += letters[Math.floor(Math.random() * 16)];
+		}
+		return colors;
+	};
+
+	return (
+		<div className='container m-auto text-center'>
+			<Title text={'Randomise Colors'} classes={'mb-4'} />
+			<button className='btn btn-danger' onClick={(e) => handleClick(e)}>
+				Click me
+			</button>
+			<button className='btn btn-success' onClick={handleSecClick}>
+				Click me
+			</button>
+			<button className='btn btn-primary' onClick={handleSecClick}>
+				Click me
+			</button>
+			<button className='btn btn-warning' onClick={handleSecClick}>
+				Click me
+			</button>
+		</div>
+	);
+};
+
+export default RandomiseColors;
